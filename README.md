10cee83890a727c6c8484eb5d1839e472e39d4de
Sotiri Georgiou <tirigeorgiou@gmail.com>
2020-11-07 14:57:07 +0000

    First init

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Shell Examples
+
+### References
+
+- https://blog.balthazar-rouberol.com/text-processing-in-the-shell
+
---
+++ examples/pasteColumns.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Pastes two columns together with del ','
+paste -d, ../data/ingrediants.data ../data/prices.data
